c43ea8ba441581ddb9a94b492032a9ddeafd5d14
Sh1Sh0 <98108702+Sh1Sh0p@users.noreply.github.com>
2022-01-29 13:58:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace AlphabeticLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many letters do you want?");
+            
+            var n = int.Parse(Console.ReadLine());
+            
+            char[] letters = new char[n];
+
+            Console.WriteLine($"Type {n} letters from Alphabet...");
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Here are they alphabetic:");
+
+            Array.Sort(letters);
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                Console.WriteLine(letters[i]);
+            }
+        }
+    }
+}
